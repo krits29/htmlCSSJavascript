97a51f5f57ec1afa35c60f9555e640f477a2b8d1
krits29 <31025768+krits29@users.noreply.github.com>
2020-07-06 22:54:34 -0700

    Creating objects

---
+++ basics/JS-Objects/script.js
@@ -0,0 +1,10 @@
+var popStar = {
+    firstName: "Taylor",
+    lastname: "Swift",
+    age: 30,
+    albums: ["Lover", "Reputation", "Red"],
+    isMarried: false
+};
+
+console.log(popStar.firstName);
+console.log(popStar["lastname"]);

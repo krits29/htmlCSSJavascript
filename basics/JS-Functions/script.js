c42b216a893c3079af63f4ac0a2474c220774c53
krits29 <31025768+krits29@users.noreply.github.com>
2020-07-08 20:23:10 -0700

    Functions Arrays Loops

--- basics/JS-Functions/script.js
+++ basics/JS-Functions/script.js
@@ -74,3 +74,77 @@ function completeBill (bill1, bill2, bill3) {
 console.log(allTips(124, 48, 268));
 console.log(completeBill(124, 48, 268));
 
+console.log("___________________");
+console.log("CHALLENGE - more advanced");
+
+var restaurantBillJohn = {
+    billValues: [],
+    tips: [],
+    completeBill: [],
+    tipCalculator: function () {
+        for(var i = 0; i < this.billValues.length; i++) {
+            if(this.billValues[i] < 50) {
+                this.tips[i] = (0.2 * this.billValues[i]);
+                this.completeBill[i] = (0.2 * this.billValues[i]) + this.billValues[i];
+            } else if(this.billValues[i] >= 50 && this.billValues[i] < 200) {
+                this.tips[i] = (0.15 * this.billValues[i]);
+                this.completeBill[i] = (0.15 * this.billValues[i]) + this.billValues[i];
+            } else {
+                this.tips[i] = (0.1 * this.billValues[i]);
+                this.completeBill[i] = (0.1 * this.billValues[i]) + this.billValues[i];
+            }
+        }
+    }
+};
+
+restaurantBillJohn.billValues = [124, 48, 268, 180, 42];
+restaurantBillJohn.tipCalculator();
+console.log(restaurantBillJohn.tips);
+console.log(restaurantBillJohn.completeBill);
+
+console.log("________________");
+
+var restaurantBillMark = {
+    billValues: [],
+    tips: [],
+    completeBill: [],
+    tipCalculator: function () {
+        for(var i = 0; i < this.billValues.length; i++) {
+            if(this.billValues[i] < 100) {
+                this.tips[i] = (0.2 * this.billValues[i]);
+                this.completeBill[i] = (0.2 * this.billValues[i]) + this.billValues[i];
+            } else if(this.billValues[i] >= 100 && this.billValues[i] < 300) {
+                this.tips[i] = (0.1 * this.billValues[i]);
+                this.completeBill[i] = (0.1 * this.billValues[i]) + this.billValues[i];
+            } else {
+                this.tips[i] = (0.25 * this.billValues[i]);
+                this.completeBill[i] = (0.25 * this.billValues[i]) + this.billValues[i];
+            }
+        }
+    }
+};
+
+restaurantBillMark.billValues = [77, 375, 110, 45];
+restaurantBillMark.tipCalculator();
+console.log(restaurantBillMark.tips);
+console.log(restaurantBillMark.completeBill);
+
+function averageTips(tips) {
+    var total = 0;
+    for(var i = 0; i < tips.length; i++) {
+        total += tips[i];
+    }
+    return total/tips.length;
+}
+
+console.log(averageTips(restaurantBillMark.tips));
+console.log(averageTips(restaurantBillJohn.tips));
+
+if(averageTips(restaurantBillMark.tips) > averageTips(restaurantBillJohn.tips)) {
+    console.log("Mark's family paid a higher average of tips");
+} else if(averageTips(restaurantBillJohn.tips) > averageTips(restaurantBillMark.tips)) {
+    console.log("John's family paid a higher average of tips");
+} else {
+    console.log("They both paid the same amount of tips");
+}
+
